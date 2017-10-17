563608b4adf7d229a521a9002e28b719a1e7147c
Brandon Roberts <brandon@bxroberts.org>
2017-10-16 17:37:26 -0700

    simplify syntax

--- src/main/scala/InteractiveParser.scala
+++ src/main/scala/InteractiveParser.scala
@@ -1,3 +1,4 @@
+package org.bxroberts.parse
 
 import org.apache.nutch.parse.Outlink
 import org.apache.nutch.parse.Parser
@@ -13,29 +14,27 @@ import org.slf4j.LoggerFactory
 
 import java.lang.invoke.MethodHandles
 
-package org.bxroberts.parse {
-  class InteractiveParser extends Parser {
-    def getParse(content: Content): ParseResult = {
-      log.info(s"URL: ${content.getUrl}")
-      log.info(s"BASE: ${content.getBaseUrl}")
-      //log.info(s"CONTENT ${content.toString}")
-      log.info(content.toString)
+class InteractiveParser extends Parser {
+  def getParse(content: Content): ParseResult = {
+    log.info(s"URL: ${content.getUrl()}")
+    log.info(s"BASE: ${content.getBaseUrl()}")
+    log.info(s"CONTENT ${content.toString().length}")
+    log.info(content.toString())
 
-      val status = new ParseStatus(ParseStatus.SUCCESS)
+    val status = new ParseStatus(ParseStatus.SUCCESS)
 
-      // var outlink = new Outlink(
-      //   "intact://http://lol.com$$CLICK['btnsearch']", "Search"
-      // )
-      // val outlinks: Array[Outlink] = Array(outlink)
-      return status.getEmptyParseResult(content.getUrl(), getConf())
-    }
-
-    // getters setters interface >:|
-    var conf: Configuration = NutchConfiguration.create()
-    def setConf(new_conf: Configuration ): Unit = (conf = new_conf)
-    def getConf(): Configuration = conf
-
-    // logging
-    val log: Logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    // var outlink = new Outlink(
+    //   "intact://http://lol.com$$CLICK['btnsearch']", "Search"
+    // )
+    // val outlinks: Array[Outlink] = Array(outlink)
+    return status.getEmptyParseResult(content.getUrl(), getConf())
   }
+
+  // getters setters interface >:|
+  var conf: Configuration = NutchConfiguration.create()
+  def setConf(new_conf: Configuration ): Unit = (conf = new_conf)
+  def getConf(): Configuration = conf
+
+  // logging
+  val log: Logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 }
