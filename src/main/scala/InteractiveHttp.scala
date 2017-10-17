966079a01d234265e6d8fb446ceefac43a353454
Brandon Roberts <brandon@bxroberts.org>
2017-10-16 21:23:40 -0700

    logging, combined install

--- src/main/scala/InteractiveHttp.scala
+++ src/main/scala/InteractiveHttp.scala
@@ -1,6 +1,5 @@
-package org.bxroberts.http
+package org.bxroberts.protocol.http
 
-import java.net.URL
 import collection.mutable.HashMap
 
 import org.apache.hadoop.io.Text
@@ -13,35 +12,56 @@ import org.apache.nutch.protocol.http.api.HttpBase
 import org.apache.nutch.protocol.http.api.HttpException
 import org.apache.nutch.net.protocols.Response
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+
+import java.net.URL
+import java.lang.invoke.MethodHandles
+
+
+
 class InteractiveHttpResponse(url: URL, datum: CrawlDatum) extends Response {
   var code: Int = 0
   var headers: HashMap[String, String] = HashMap.empty
-  var content: Array[Byte] = Array[Byte]()
+  // setHeader = headers.put(Key, Value)
+  var content: String = ""
 
-  //public URL getUrl();
+  /**
+   * AS PROTOCOL
+   * This follows iac links and grabs whatever lies at the end of it. The thinking
+   * here is that if a clickable results in a list with a bunch of plain html hrefs
+   * that normal plugins can parse that and we can also constrct more iac links if
+   * we need to from it.
+   * - if url not iac://, bail
+   * - load webdriver
+   * - follow iac path
+   * - return content from data at end of iac path
+   */
+
+  // JAVA INTERFACE
+  /** Get original URL **/
   def getUrl(): URL = url
 
   /** Returns the response code. */
-  //public int getCode();
   def getCode(): Int = code
 
   /** Returns the value of a named header. */
-  //public String getHeader(String name);
   def getHeader(name: String): String = headers(name)
 
   /** Returns all the headers. */
-  //public Metadata getHeaders();
   def getHeaders(): Metadata = new Metadata()
 
   /** Returns the full content of the response. */
-  //public byte[] getContent();
-  def getContent(): Array[Byte] = content
-
+  def getContent(): Array[Byte] = content.map(_.toByte).toArray
 }
 
 class InteractiveHttp extends HttpBase {
   def getResponse(url: URL, datum: CrawlDatum, redirect: Boolean): Response = {
+    log.info("HEEEEEEEEEEEELLLLLLLLLLLLLLLLLLLLLLOOOOO InteractiveHttp")
     new InteractiveHttpResponse(url, datum)
   }
+
+  // logging
+  val log: Logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 }
 
